efac55496dbb9691b3eb504f93d59823a080803d
samu7861 <sfrancog7861@gmail.com>
2024-03-27 22:28:58 -0500

    HomePage

---
+++ khanban/lib/home/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:khanban/utils/colors.dart'; 
+import 'package:khanban/widgets/infoProyect.dart';
+import 'package:khanban/widgets/next.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(201, 231, 242, 1),
+      body: Column(
+        children: [
+          SizedBox(height: 50,),
+          Center(
+            child: Container(
+              width: 195,
+              height: 50,
+              decoration: ShapeDecoration(
+                  gradient: LinearGradient(
+                  begin: Alignment(1.00, 0.03),
+                  end: Alignment(-1, -0.03),
+                  colors: [ Color.fromRGBO(10, 73, 111, 1), Color.fromRGBO(118, 191, 235, 1)],
+                    ),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                ),
+              child: Center(
+                child: Text(
+                  'BOARD',
+                  style: GoogleFonts.monomaniacOne(
+                    fontSize: 24,
+                    color: AppColors.azulOscuro,
+                  ),
+                ),
+              ),
+              )
+          )
+        ],
+      )
+    );
+  }
+}
--- khanban/lib/widgets/next.dart
+++ khanban/lib/widgets/next.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:khanban/utils/colors.dart'; 
+import 'package:khanban/home/home_page.dart'; 
 
 class Next extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      width: 82,
-      decoration: BoxDecoration(
-        color: AppColors.azulOscuro,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Center(
-        child: SvgPicture.asset("assets/icons/arrowright.svg", height: 45, width: 45, color: AppColors.celeste,),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage(title: "khanban",)),
+        );
+      },
+      child: Container(
+        height: 56,
+        width: 82,
+        decoration: BoxDecoration(
+          color: AppColors.azulOscuro,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: SvgPicture.asset("assets/icons/arrowright.svg", height: 45, width: 45, color: AppColors.celeste,),
+        ),
       ),
     );
   }
